756e8ff6b074c10f2650021c73bc01ef88c41ab4
abhiiticlass-star <abhiiticlass@gmail.com>
2026-06-03 09:24:40 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
-API_KEY = "N8I9XB3N21L0RJ40"
+API_KEY = "8e9f4f263cd044cdb3a0a6972179737a"
 
-BASE_URL = "https://www.alphavantage.co/query"
+BASE_URL = "https://api.twelvedata.com"
 
 PAIRS = ["EURUSD", "GBPUSD", "USDJPY", "AUDUSD"]
